fd6f61fef0358990d719c77c8ddbd0f5f366af99
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-06-03 14:48:08 +0100

    add logging

--- src/app/lib/storage/write-file-blob.ts
+++ src/app/lib/storage/write-file-blob.ts
@@ -1,6 +1,8 @@
 import { put } from "@vercel/blob";
 
 export const writeFileBlob = async (data: Data, filename: string) => {
+    console.log(`Writing ${filename} to blob store...`)
     const blob = await put(filename, JSON.stringify(data), { access: "public", allowOverwrite: true, addRandomSuffix: true });
+    console.log(`Successfully written ${filename} to blob store.`)
     return blob;
 };
